61f952a09593c0b1cdbf34c24d9376aad7d0c544
Akari <akka@mac.local>
2026-07-16 01:33:29 +0900

    Restore and fix db.js with correct Sequelize setup

--- src/db.js
+++ src/db.js
@@ -1,52 +1,18 @@
-import { Client, GatewayIntentBits, Events } from "discord.js";
+import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-import sequelize from "./db.js";
-import Task from "./models/Task.js";
-import { help } from "./commands/help.js";
-import { add } from "./commands/add.js";
-import { list } from "./commands/list.js";
-import { done } from "./commands/done.js";
-import { week } from "./commands/week.js"; // 追記箇所①
 
 dotenv.config();
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+// データベースへの接続設定
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false, // ログをすっきりさせるために非表示
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Renderのデータベース接続に必要な設定です
+    },
+  },
 });
 
-client.once(Events.ClientReady, async (readyClient) => {
-  await sequelize.sync();
-  console.log("データベース接続成功");
-  console.log(`${readyClient.user.tag} としてログインしました`);
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === "help") {
-    await help(interaction);
-  }
-  
-  if (interaction.commandName === "add") {
-    await add(interaction);
-  }
-
-  if (interaction.commandName === "list") {
-    await list(interaction);
-  }
-
-  if (interaction.commandName === "done") {
-    await done(interaction);
-  }
-
-  // 追記箇所②
-  if (interaction.commandName === "week") {
-    await week(interaction);
-  }
-});
-
-client.login(process.env.DISCORD_TOKEN);
+export default sequelize;
